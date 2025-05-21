9cbfd5f713a3fe85cd894f3c9365c13f3305ba4b
Nicholas Winterhalter <78866820+Gewinum@users.noreply.github.com>
2025-05-21 12:17:22 +0400

    feat: play button

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -2,6 +2,7 @@ import type {Nothing} from "../utils/nothing.ts";
 import * as React from "react";
 import "../style/description.css";
 import catLogo from "../assets/about-game.svg";
+import {toast} from "react-toastify";
 
 class Description extends React.Component<Nothing, Nothing> {
     render() {
@@ -11,6 +12,7 @@ class Description extends React.Component<Nothing, Nothing> {
                 <h3 className="poppins-extralight-italic">
                     где ты можешь почувствовать себя настоящим диким и дерзким котом-одиночкой, умным и дипломатичным Вожаком, несмышлёным и забавным котёнком, а может и озлобленным на мир духом Тьмы?
                 </h3>
+                <button onClick={() => toast.warning("Игра пока что в разработке, следите за нашим ВК")}>Играть</button>
             </div>
             <div className="cat-logo">
                 <img src={catLogo} />
